92631f2434d0f3fbccb4d5637f40f98b419ba150
ravibharathi656 <ravibharathi656@gmail.com>
2025-07-08 15:59:48 +0530

    Updated Inventory App: a modified doctypes

--- inventory_app/inventory_app/doctype/purchase_order/purchase_order.py
+++ inventory_app/inventory_app/doctype/purchase_order/purchase_order.py
@@ -1,12 +1,16 @@
 import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate
 class PurchaseOrder(Document):
     def on_submit(self):
-        frappe.get_doc({
-			"doctype": "Stock Ledger",
-   			"item": self.item,
-      		"quantity": self.quantity,
-        	"type": "Purchase",
-         	"date": self.date,
-      		"warehouse": self.warehouse,	
-		}).insert()
+        # Create a Stock Ledger entry for Purchase
+        ledger_entry = frappe.get_doc({
+            "doctype": "Stock Ledger",
+            "item": self.item,
+            "warehouse": self.warehouse,
+            "quantity": self.quantity,  # Stock comes IN
+            "type": "Purchase",
+            "date": self.date or nowdate()
+        })
+        ledger_entry.insert()
+        frappe.msgprint("Stock Ledger updated for Purchase")
--- inventory_app/inventory_app/doctype/sales_order/sales_order.py
+++ inventory_app/inventory_app/doctype/sales_order/sales_order.py
@@ -1,12 +1,16 @@
 import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate
 class SalesOrder(Document):
     def on_submit(self):
-        frappe.get_doc({
-			"doctype": "Stock Ledger",
-   			"item": self.item,
-      		"quantity": self.quantity,
-        	"type": "Sales",
-         	"date": self.date,
-      		"warehouse": self.warehouse,	
-		}).insert()
+        # Create a Stock Ledger entry for Sales
+        ledger_entry = frappe.get_doc({
+            "doctype": "Stock Ledger",
+            "item": self.item,
+            "warehouse": self.warehouse,
+            "quantity": -self.quantity,  # Stock goes OUT
+            "type": "Sales",
+            "date": self.date or nowdate()
+        })
+        ledger_entry.insert()
+        frappe.msgprint("Stock Ledger updated for Sales")
